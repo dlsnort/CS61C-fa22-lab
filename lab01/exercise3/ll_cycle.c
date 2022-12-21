1d6bf676959383f52765fd38710df796244fd82f
jian <1184880285@qq.com>
2022-12-21 16:05:09 +0800

    lab01

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,22 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+        return 0;
+    }
+    node *fast_ptr, *slow_ptr;
+    fast_ptr = slow_ptr = head;
+    int i;
+    while (1) {
+        for (i = 0; i < 2; i++) {
+            fast_ptr = fast_ptr->next;
+            if (fast_ptr == NULL) {
+                return 0;
+            }
+        }
+        slow_ptr = slow_ptr->next;
+        if (slow_ptr == fast_ptr) {
+            return 1;
+        }
+    }
 }
