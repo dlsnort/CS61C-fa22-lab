1f903991d0045fee32b650f2dc848e17e5bff740
jian <1184880285@qq.com>
2023-04-06 08:22:02 +0800

    fa22-lab7-completed

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -13,4 +13,38 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    int a = n / 4;
+    int b = n % 4;
+    for (int i = 0; i <= a; i++) {
+        if (i != a) {
+            for (int j = 0; j <= a; j++) { 
+                for (int x = 4 * i; x < 4 * i + 4; x++) {
+                    if (j != a) {
+                        for (int y = 4 * j; y < 4 * j + 4; y++) {
+                            dst[y + x * n] = src[x + y * n];
+                        }
+                    } else {
+                        for (int y = 4 * j; y < 4 * j + b; y++) {
+                            dst[y + x * n] = src[x + y * n];
+                        }
+                    }
+
+                }
+            }
+        } else {
+            for (int j = 0; j <= a; j++) { 
+                for (int x = 4 * i; x < 4 * i + b; x++) {
+                    if (j != a) {
+                        for (int y = 4 * j; y < 4 * j + 4; y++) {
+                            dst[y + x * n] = src[x + y * n];
+                        }
+                    } else {
+                        for (int y = 4 * j; y < 4 * j + b; y++) {
+                            dst[y + x * n] = src[x + y * n];
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
